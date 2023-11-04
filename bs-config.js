2fd52979f7e45f39ddc35867bb3dbf4ffb0739c6
Iker Famoso <127440070+Iker-Famoso123@users.noreply.github.com>
2023-11-04 02:22:20 -0600

    First

---
+++ bs-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    files: ["**/*.php"],
+    proxy: "localhost", // Cambia a la URL de tu sitio local si es diferente
+    open: false, // Deshabilita la apertura automatica del navegador
+  };
+  
