b734ad55b8ddc7b1b89237226e82459f10f38db0
ikercfamoso <iker.covarrubias5286@alumnos.udg.mx>
2024-07-10 21:27:39 -0600

    Add page sell to test sales and endpoint

---
+++ src/view/resources/js/sell.js
@@ -0,0 +1,42 @@
+const urlTest = 'http://localhost:1234'
+
+const log = () => {
+    window.location.href = "./sell.html"
+}
+
+const changeQuantity = (button, delta) => {
+    const quantityElement = button.parentElement.children[1];
+    let currentQuantity = parseInt(quantityElement.innerText);
+    let newQuantity = currentQuantity + delta;
+
+    if (newQuantity >= 0) {
+        quantityElement.innerText = newQuantity;
+    }
+}
+
+const collectQuantities = () => {
+    const quantities = {}
+    document.querySelectorAll('.quantity').forEach( span => {
+        quantities[span.getAttribute('data-id')] = parseInt(span.textContent)
+    })
+
+    return quantities
+}
+
+const proceedToSale = async () => {
+    const data = collectQuantities()
+
+    fetch(urlTest + '/sales', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((res) => res.json())
+    .then((response) => console.log("Success: ", response))
+    .catch((error) => console.error("Error: ", error))
+
+
+
+}
