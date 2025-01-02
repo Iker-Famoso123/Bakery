cfffd5f08647846385450f42b7de1858374b5a28
Iker-Famoso123 <iker.covarrubias5286@alumnos.udg.mx>
2025-01-02 00:20:11 -0600

    feat: custom notifications

--- src/view/resources/js/sell.js
+++ src/view/resources/js/sell.js
@@ -7,8 +7,60 @@ const log = () => {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    var notyf = new Notyf();
-    notyf.success('¡Operación exitosa!');
+    const notyf = new Notyf({
+        duration: 3000,
+        position: {
+            x: 'right',
+            y: 'bottom'
+        },
+        types: [
+            {
+                type: 'success',
+                background: '#7367F0',
+                icon: {
+                    tagName: 'i', // Usar Font Awesome
+                    className: 'fas fa-check-circle !text-white !text-lg' // Ícono de Font Awesome con clases de estilo
+                },
+                text: 'Primary Notification!',
+                color: 'white'
+            },
+            {
+                // Info notification
+                type: 'info',
+                background: '#06B6D4',
+                icon: {
+                    tagName: 'i', // Usar Font Awesome
+                    className: 'fas fa-info-circle !text-white !text-lg' // Ícono de información
+                },
+                text: 'Info Notification!',
+                color: 'white'
+            },
+            {
+                // Warning notification
+                type: 'warning',
+                background: '#FCAA1D',
+                icon: {
+                    tagName: 'i', // Usar Font Awesome
+                    className: 'fas fa-exclamation-triangle !text-white !text-lg' // Ícono de advertencia
+                },
+                text: 'Warning Notification!',
+                color: 'white'
+            },
+            {
+                type: 'error',
+                background: '#EF4444', // Rojo intenso para indicar error
+                icon: {
+                    tagName: 'i', // Usar Font Awesome
+                    className: 'fas fa-times-circle !text-white !text-lg' // Ícono de error
+                },
+                text: 'Error Notification!',
+                color: 'white' // Color del texto
+            }
+        ]
+    });
+    
+    
+
 
     window.proceedToSale = async () => {
         const data = collectQuantities()
@@ -25,11 +77,26 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log("Success: ", response)
             document.querySelectorAll('button + input').forEach( input => {
                 input.value = 0
-            })
-            notyf.success('¡Venta registrada con éxito!');
+            });
+            notyf.open({
+                type: 'success',
+                message: 'Venta registrada con éxito',
+                duration: 3000,
+                ripple: true,
+                dismissible: true
+            });
         })
         .catch((error) => {
-            notyf.error('¡Hubo un error al registrar la venta!');
+            notyf.open({
+                type: 'error',
+                message: '¡Error al registrar la venta!',
+                duration: 3000,
+                ripple: true,
+                dismissible: true
+            });
+
+
+            
             console.error("Error: ", error);
         })
     
@@ -45,7 +112,6 @@ const changeQuantity = (button, delta) => {
         quantityElement.value = newQuantity
     }
 };
-
 
 const collectQuantities = () => {
     const quantities = {}
